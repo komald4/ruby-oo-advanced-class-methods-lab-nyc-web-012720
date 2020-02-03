db122c771c6728e60f71a22fc0604d96f05c6fa8
Komal Dindiyal <komald.4@gmail.com>
2020-02-03 14:34:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,14 @@ class Song
     song
   end
 
-def self.find_by_name(song_name)
-  @@all.find {|song| song.name == song_name} # find each song and matches
-end
+  def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name} # find each song and matches
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
 
   def self.all
     @@all
