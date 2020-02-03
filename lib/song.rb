91d62f7ec55dfe22e405ec6f4b2d7becdcf2ff18
Komal Dindiyal <komald.4@gmail.com>
2020-02-03 15:08:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     song
   end
 
-  def create_from_filename
+  def self.create_from_filename
     info = filename.split(" - ")
     song = self.create #created a new song object
     song.name = info[1].chomp(".mp3")#remove .mp3 on song name
